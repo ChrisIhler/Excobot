817272d744fcc54e96f7b1cbdf07f431d1fff67d
ChrisIhler <Ihlerconsulting@gmail.com>
2019-04-27 11:36:23 -0700

    updated packages and server

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+app.disable('x-powered-by')
+app.use(cors())
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').load()
+}
+
+// app.use('/users', require('./routes/users'))
+
+app.use((err, req, res, next) => {
+  console.log(err)
+  const status = err.status || 500
+  res.status(status).json({ error: err, message: 'app.js' })
+})
+
+app.use((req, res, next) => {
+  console.log(req)
+  res.status(404).json({ error: { message: 'Not Found app.js' }})
+})
+
+const listener = () => console.log(`Listening on port ${port}!`)
+app.listen(port, listener)
